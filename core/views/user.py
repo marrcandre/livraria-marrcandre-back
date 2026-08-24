105eeeb6939179c0e70e6fe2abb048f4d9a7ec11
Marco André Mendes <marcoandre@gmail.com>
2026-08-24 15:56:23 -0300

    feat: adicionado o método PATCH para /usuarios/me paa permitir edição do perfil do usuário

--- core/views/user.py
+++ core/views/user.py
@@ -16,13 +16,18 @@ class UserViewSet(ModelViewSet):
 
     @extend_schema(
         summary="Dados do usuário autenticado",
-        description="Retorna os dados do usuário autenticado.",
+        description="Retorna ou atualiza os dados do usuário autenticado.",
         responses={200: UserSerializer, 401: None},
     )
-    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['get', 'patch'], permission_classes=[IsAuthenticated])
     def me(self, request):
-        """ Retorna os dados do usuário autenticado."""
+        """ Retorna ou atualiza os dados do usuário autenticado."""
         user = request.user
+        if request.method == 'PATCH':
+            serializer = UserSerializer(user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
